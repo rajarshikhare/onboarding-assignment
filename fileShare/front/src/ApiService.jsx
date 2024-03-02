557e5298071ee90b03433e427ae344df1d0c189d
rajarshikhare <rajarshi.vaibhav@gmail.com>
2024-03-02 20:32:00 +0530

    bug fixes

--- fileShare/front/src/ApiService.jsx
+++ fileShare/front/src/ApiService.jsx
@@ -17,7 +17,7 @@ export const signUp = (user) => {
   return authCalls.post(`/signup`, user).then((r) => {
     localStorage.setItem("token", r.data.data.token);
     localStorage.setItem("name", r.data.data.user.name);
-    localStorage.setItem("name", r.data.data.user.email);
+    localStorage.setItem("email", r.data.data.user.email);
     initAuth();
     return r.data;
   });
